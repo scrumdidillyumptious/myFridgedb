038445e1ada90093fd85a9db1de5e05d63fbfb59
benmxavier <benmxavier@gmail.com>
2017-02-28 12:20:04 -0500

    Added the test data for users

---
+++ testDummyDataUsers.sql
@@ -0,0 +1,9 @@
+use myFridgeDB;
+insert into users(userid,username, password) 
+value(0001,'Ben' , 'password');
+insert into users(userid,username, password) 
+value(0002,'Sam' , 'password');
+
+insert into users(userid,username, password) 
+value(0003,'John' , 'password');
+
