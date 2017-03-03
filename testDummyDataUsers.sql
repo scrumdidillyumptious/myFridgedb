a13e002b3d23468a69a10ca86a694dea2a64fd11
Ben <benjamin.xavier@mymail.champlain.edu>
2017-03-02 20:20:52 -0500

    added dummy user to work with test cases

--- testDummyDataUsers.sql
+++ testDummyDataUsers.sql
@@ -1,9 +1,10 @@
 use myFridgeDB;
-insert into users(userid,username, password) 
+insert into users(userid,username, password)
 value(0001,'Ben' , 'password');
-insert into users(userid,username, password) 
+insert into users(userid,username, password)
 value(0002,'Sam' , 'password');
 
-insert into users(userid,username, password) 
+insert into users(userid,username, password)
 value(0003,'John' , 'password');
-
+insert into users(userid,username, password) 
+value(0004,'testUserName' , 'password');
